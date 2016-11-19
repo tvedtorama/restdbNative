b57d7fa77c2b818f43a765c5643569492d30fa9b
Jørgen Tvedt <tvedtorama@gmail.com>
2016-11-19 09:45:18 +0100

    Loading tasks at init, using idToken. (This needs login to be enabled, or fake idToken).

--- app/sagas/mainLoop.ts
+++ app/sagas/mainLoop.ts
@@ -2,7 +2,9 @@ import * as _ from 'lodash'
 import {takeEvery} from 'redux-saga'
 import {put, take, select, call} from 'redux-saga/effects'
 
-import {IState} from '../IState'
+import {get, post} from 'axios'
+
+import {IState, ITask} from '../IState'
 
 // Actions from saga to reducer 
 export const ADD_TASK = "ADD_TASK"
@@ -18,11 +20,12 @@ export const REQUEST_SET_ALL_TASKS_COMPLETION = "REQUEST_SET_ALL_TASKS_COMPLETIO
 export const REQUEST_DELETE_TASK = "REQUEST_DELETE_TASK"
 export const REQUEST_CLEAR_COMPLETED = "REQUEST_CLEAR_COMPLETED"
 
-export function* requestAddTask(action: {text: string}) {
+export function* requestAddTask(idToken, action: {text: string}) {
+	console.log("reqAddTask", action)
 	yield put({type: ADD_TASK, text: action.text, taskId: Date.now().toString(), isDone: false})
 }
 
-export function* requestSetTaskCompletion(action: {id: string, isDone?: boolean}) : Iterator<any> {
+export function* requestSetTaskCompletion(idToken, action: {id: string, isDone?: boolean}) : Iterator<any> {
 	const isDone = _.isUndefined(action.isDone) ? 
 		!_((<IState>(yield select())).tasks).filter(x => x.taskId === action.id).head().isDone : 
 		action.isDone
@@ -30,7 +33,7 @@ export function* requestSetTaskCompletion(action: {id: string, isDone?: boolean}
 	yield put({type: SET_TASK_COMPLETION, id: action.id, isDone})
 }
 
-export function* requestSetAllTasksCompletion(action: {isDone?: boolean}) : Iterator<any> {
+export function* requestSetAllTasksCompletion(idToken, action: {isDone?: boolean}) : Iterator<any> {
 	const {isDone} = action
 
 	const wrongStateItems = _((<IState>(yield select())).tasks).filter(x => x.isDone !== isDone)
@@ -38,30 +41,52 @@ export function* requestSetAllTasksCompletion(action: {isDone?: boolean}) : Iter
 	yield wrongStateItems.map(x => call(requestSetTaskCompletion, {id: x.taskId, isDone})).value()
 }
 
-export function* requestDeleteTask(action: {id: string}) : Iterator<any> {
+export function* requestDeleteTask(idToken, action: {id: string}) : Iterator<any> {
 	yield put({type: DELETE_TASK, id: action.id})
 }
 
-export function* requestClearCompleted(action: {}): Iterator<any> {
+export function* requestClearCompleted(idToken, action: {}): Iterator<any> {
 	const wrongStateItems = _((<IState>(yield select())).tasks).filter(x => x.isDone)
 
 	yield wrongStateItems.map(x => call(requestDeleteTask, {id: x.taskId})).value()	
 }
 
+function getHeader(dbData, idToken) {
+	const apiKey = idToken ? null : { "x-apikey": dbData.apiKey }
+	const token = idToken ? { 'Authorization': 'Bearer ' + idToken } : null
+	return { headers: Object.assign({}, apiKey, token) }
+}
+
+const collection = "task"
+
+const dbData = { url: "https://reactnative-ec9e.restdb.io", apiKey: "582aa43e178b07f36f7e5043" }
+
+function loadTasks(params, idToken) {
+	return get(`${dbData.url}/rest/${collection}`, Object.assign(getHeader(dbData, idToken), {
+		params
+	})).then(x => {
+		return x.data
+	}).catch(err => {
+		console.error("error in axios", err)
+	})
+}
+
 export function* mainLoop() : any {
-	const action = yield take(LOGIN_OK)
+	const action = yield take(LOGIN_OK) // Waits for login to complete
 	
-	console.log("all is OK! ", action)
+	const idToken = action.idToken
+	yield put({type: SET_LOGIN_DATA, idToken})
 
-	yield put({type: SET_LOGIN_DATA, idToken: action.idToken})
+	const loadedTasks = <ITask[]>(yield call(loadTasks, {}, idToken))
+	yield loadedTasks.map(x => put(Object.assign({}, x, {type: ADD_TASK})))
 
-	yield takeEvery(REQUEST_ADD_TAKS, requestAddTask)
-	yield takeEvery(REQUEST_SET_TASK_COMPLETION, requestSetTaskCompletion)
-	yield takeEvery(REQUEST_SET_ALL_TASKS_COMPLETION, requestSetAllTasksCompletion)
-	yield takeEvery(REQUEST_DELETE_TASK, requestDeleteTask)
-	yield takeEvery(REQUEST_CLEAR_COMPLETED, requestClearCompleted)
+	yield takeEvery(REQUEST_ADD_TAKS, requestAddTask, idToken)
+	yield takeEvery(REQUEST_SET_TASK_COMPLETION, requestSetTaskCompletion, idToken)
+	yield takeEvery(REQUEST_SET_ALL_TASKS_COMPLETION, requestSetAllTasksCompletion, idToken)
+	yield takeEvery(REQUEST_DELETE_TASK, requestDeleteTask, idToken)
+	yield takeEvery(REQUEST_CLEAR_COMPLETED, requestClearCompleted, idToken)
 	
-	yield put({type: REQUEST_ADD_TAKS, text: "Hei"})
+//	yield put({type: REQUEST_ADD_TAKS, text: "Hei"})
 
 /*	while (true) {
 		const task = <{text: string}>(yield take(REQUEST_ADD_TAKS))
