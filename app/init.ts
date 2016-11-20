9f964605b3e27c2b0e4c08b5b9c910e48f6fa26f
Jørgen Tvedt <tvedtorama@gmail.com>
2016-11-20 09:43:24 +0100

    Actual login logic activated, remote-devtools deactivated.

--- app/init.ts
+++ app/init.ts
@@ -10,19 +10,15 @@ const {composeWithDevTools} = RemoteDevTools
 const devToolsEnhancer = RemoteDevTools.default
 
 export function createReduxStore() {
-	console.log("are there huge issues in here?")
-
-
 	// This causes the remote dev tool to use remotedev.io, which can be accessed at remotedev.io/local
-	// const composeEnhancers = composeWithDevTools({ realtime: true }) 
+	const composeEnhancers = composeWithDevTools({ realtime: true }) 
 	
 	const middleware = createSagaMiddleware()
 	const store = createStore(mainReducer, applyMiddleware(middleware)) // composeEnhancers(applyMiddleware(middleware)))
 
 	middleware.run(mainLoop)
 
-	store.dispatch({type: LOGIN_OK, idToken: 'FAKE'}) // Skip login for now
+	// store.dispatch({type: LOGIN_OK, idToken: "XXXXXXXXXXX"}) // Insert a valid idToken and uncomment to skip the login process
 
 	return store
 }
-
